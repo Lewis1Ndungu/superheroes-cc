9ebf10e2d7d70767d991c92da078a9c3a2cd6697
Lewis1Ndungu <lewis.ndungu@student.moringaschool.com>
2023-03-28 11:00:16 +0300

    Done

---
+++ app/controllers/heros_controller.rb
@@ -0,0 +1,26 @@
+class HerosController < ApplicationController
+
+    def index
+        heroes = Hero.all 
+        render json: heroes 
+        # each_serializer: HeroSerializer 
+    end
+
+    def show
+        hero = Hero.find_by(id: params[:id])
+        if hero 
+            render json: hero, except: [:created_at, :updated_at]
+            # , serializer: HeroSerializer
+        else 
+            render json: { error: "Hero not found"}, status: :not_found
+        end
+    end
+    def create
+        hero = Hero.create!(hero_params)
+        render json: hero 
+    end
+    private
+    def hero_params
+        params.permit(:name, :super_name)
+    end
+end
